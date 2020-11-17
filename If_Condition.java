b91a1426eaa933eb933979db2ce3b43900b3eb36
darshanabk <74397181+darshanabk@users.noreply.github.com>
2020-11-17 21:04:52 +0530

    Add files via upload

---
+++ If_Condition.java
@@ -0,0 +1,16 @@
+package com.java_tutorial;
+
+public class If_Condition {
+    public static int a = 10;
+    public static int b = 20;
+    public static void main(String[] args) {
+        if (a<b) {
+            System.out.println("a is lesser than b");
+        }
+    }
+}
+
+
+
+
+
